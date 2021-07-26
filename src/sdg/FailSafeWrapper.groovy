fd93eb3ba7185cb4bbd4f510a47edcf6f7b71f4b
Paller <KimChesed.Paller@analog.com>
2021-07-26 09:05:23 +0800

    fix linux stage and add more debugging info

--- src/sdg/FailSafeWrapper.groovy
+++ src/sdg/FailSafeWrapper.groovy
@@ -28,11 +28,13 @@ class FailSafeWrapper {
                 delegate.invokeMethod(name, args)
             }catch (Exception ex){
                 echo "Stage failed. Error ${ex.getMessage()}"
+                echo getStackTrace(ex)
                 if (nextDelegate) {
                     try {
                         nextDelegate.invokeMethod(name, args)
                     }catch (Exception ex_d){
                         echo "Delegated stage failed. Error ${ex_d.getMessage()}"
+                        echo getStackTrace(ex_d)
                     }
                 }
                 throw ex
@@ -41,11 +43,21 @@ class FailSafeWrapper {
             try{
                 delegate.invokeMethod(name, args)
             }catch(NominalException ex){
+                echo getStackTrace(ex)
                 unstable("Stage is unstable. Reason: ${ex.getMessage()}")  
             }catch (Exception ex){
                 echo "Stage failed. Error ${ex.getMessage()}"
+                echo getStackTrace(ex)
                 throw ex
             }
         }
     }
+    private def String getStackTrace(Throwable aThrowable){
+        // Utility method to print the stack trace of an error
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos, true);
+        aThrowable.printStackTrace(ps);
+        return baos.toString();
+    }
+
 }
