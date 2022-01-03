82bed6bf2c26300d186f3f526fec03def5efd00b
Paller <KimChesed.Paller@analog.com>
2022-01-03 23:37:43 +0800

    add stage for loading and unloading overlay files

--- vars/getGauntlet.groovy
+++ vars/getGauntlet.groovy
@@ -1,5 +1,5 @@
-def call(hdlBranch="NA", linuxBranch="NA", bootPartitionBranch="release",firmwareVersion="NA", bootfile_source="artifactory") {
+def call(hdlBranch="NA", linuxBranch="NA", bootPartitionBranch="release",firmwareVersion="NA", bootfile_source="artifactory", rpi_branch="NA") {
     def harness =  new sdg.Gauntlet()
-    harness.construct(hdlBranch, linuxBranch, bootPartitionBranch, firmwareVersion, bootfile_source)
+    harness.construct(hdlBranch, linuxBranch, bootPartitionBranch, firmwareVersion, bootfile_source, rpi_branch)
     return harness
 }
