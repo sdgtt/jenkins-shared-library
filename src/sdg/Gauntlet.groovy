94a530b9a8bd98a479f2a4b91861c3ac5a6a8a69
Trecia Agoylo <trecia.agoylo@analog.com>
2022-06-24 10:11:28 +0800

    no-os: fix directory issue for non multibranch pipeline
    Signed-off-by: Trecia Agoylo <trecia.agoylo@analog.com>

--- src/sdg/Gauntlet.groovy
+++ src/sdg/Gauntlet.groovy
@@ -700,11 +700,10 @@ def stage_library(String stage_name) {
                             println("Not a multibranch pipeline. Cloning "+gauntEnv.no_os_branch+" branch from "+gauntEnv.no_os_repo)
                             retry(3) {
                                 sleep(2)
-                                sh 'git clone --recursive -b '+gauntEnv.no_os_branch+' '+gauntEnv.no_os_repo+''
+                                sh 'git clone --recursive -b '+gauntEnv.no_os_branch+' '+gauntEnv.no_os_repo+' .'
                             }
                         }
                     }
-                    
                     sh 'cp '+pwd+'/outs/' +file+ ' no-OS/projects/'+ project +'/'
                     dir('no-OS'){
                         if (gauntEnv.vivado_ver == '2020.1'){
