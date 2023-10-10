3d586e19e4b8056ec28385a9b1684dd8ea5c5070
Travis F. Collins <travis.collins@analog.com>
2023-10-10 17:16:35 -0600

    Update setup_jenkins_agent_on_vagrant_vm.groovy

--- vars/setup_jenkins_agent_on_vagrant_vm.groovy
+++ vars/setup_jenkins_agent_on_vagrant_vm.groovy
@@ -1,5 +1,6 @@
-def call(String IP) {
+def call(String IP, Integer Port=8080) {
     sh 'wget https://gist.githubusercontent.com/ccraluca/1a031a1bd71ede37bac01c10c66197c3/raw/da35163cefc6a3ad8e137ce6cfb6831c6f884f5c/jenkins_win_vm_setup.ps1 -O jenkins_win_vm_setup.ps1'
     sh 'vagrant upload jenkins_win_vm_setup.ps1 "C:\"'
-    sh "vagrant winrm --command \"C:\\jenkins_win_vm_setup.ps1 http://\'${IP}\':8080/swarm/swarm-client.jar win-vm http://\'${IP}\':8080/ C:\\jenkins\""
+    SPort = Integer.toString(Port)
+    sh "vagrant winrm --command \"C:\\jenkins_win_vm_setup.ps1 http://\'${IP}\':\'${SPort}\'/swarm/swarm-client.jar win-vm http://\'${IP}\':\'${SPort}\'/ C:\\jenkins\""
 }
