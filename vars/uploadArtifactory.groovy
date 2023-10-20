45b2d0d64e12fb4f57f0e7ee154c3b1ce5c4a158
Travis F. Collins <travis.collins@analog.com>
2023-10-20 14:44:08 -0600

    Update uploadArtifactory.groovy

--- vars/uploadArtifactory.groovy
+++ vars/uploadArtifactory.groovy
@@ -161,3 +161,19 @@ def shellout(command) {
 
     return val
 }
+
+def checkOs() {
+    if (isUnix()) {
+        def uname = sh script: 'uname', returnStdout: true
+        if (uname.startsWith('Darwin')) {
+            return 'Macos'
+        }
+        // Optionally add 'else if' for other Unix OS
+        else {
+            return 'Linux'
+        }
+    }
+    else {
+        return 'Windows'
+    }
+}
