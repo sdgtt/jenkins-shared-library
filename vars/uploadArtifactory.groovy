0d8c223b127587253ce1d84478310c410b1980a1
Travis F. Collins <travis.collins@analog.com>
2023-10-20 14:55:04 -0600

    Update uploadArtifactory.groovy

--- vars/uploadArtifactory.groovy
+++ vars/uploadArtifactory.groovy
@@ -86,16 +86,20 @@ def call(project, filepattern) {
   //  TransceiverToolbox/release/trx-toolbox-tag/<files>
 
     // Check if we have files to upload based on target
-    if (checkOs() == 'Windows') {
-        bat 'for %A in (' + filepattern + ') do @echo %A > files_searched'
-    }
-    else {
-        sh 'ls ' + filepattern + ' > files_searched || true'
-    }
-    def files_list = readFile('files_searched')
-    println('Files found: ' + files_list)
-    if (files_list.length() <= 0) {
-        return
+    try {
+        if (checkOs() == 'Windows') {
+            bat 'for %A in (' + filepattern + ') do @echo %A > files_searched'
+        }
+        else {
+            sh 'ls ' + filepattern + ' > files_searched || true'
+        }
+        def files_list = readFile('files_searched')
+        println('Files found: ' + files_list)
+        if (files_list.length() <= 0) {
+            return
+        }
+    } catch(Exception ex) {
+        println(ex);
     }
 
     echo '-------Artifactory Git Hash lookup-------'
