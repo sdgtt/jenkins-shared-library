dad19cc76d410fd2a53065e2a140556575dbc13d
Julia Pineda <Julia.Pineda@analog.com>
2024-01-15 09:49:01 +0800

    Update telemetry installation
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

--- src/sdg/Gauntlet.groovy
+++ src/sdg/Gauntlet.groovy
@@ -1889,7 +1889,7 @@ private def install_telemetry(update_requirements=false){
             if (update_requirements){
                 run_i('pip3 install -r requirements.txt', true)
             }
-            run_i('python3 setup.py install', true)
+            run_i('pip3 install .', true)
         }
     }
 }
