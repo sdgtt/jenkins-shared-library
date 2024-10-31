3ed85fad780312c4c29a3a55859860f32197f80f
Pineda <Julia.Pineda@analog.com>
2024-10-31 12:18:29 +0800

    Parse nebula log
    Signed-off-by: Pineda <Julia.Pineda@analog.com>

--- src/sdg/Gauntlet.groovy
+++ src/sdg/Gauntlet.groovy
@@ -981,7 +981,7 @@ def stage_library(String stage_name) {
                 def lines = []
                 def file = new File(board + '_.log')
                 try {
-                    lines = file.readLines()
+                    lines = file..text.readLines()
                 } catch (IOException e) {
                     println("Error reading nebula command log file: ${e.message}")
                 }
@@ -995,12 +995,11 @@ def stage_library(String stage_name) {
                 def keys = ['total', 'used', 'free', 'shared', 'buff/cache', 'available']
                 def memory_type = ['Mem:', 'Swap:']
 
-                for (i = 0; i < lines.size(); i++) {
-                    // if (lines[i].contains('git')) {
-                    //     // matches = (line =~ /\<(.*?)>/).findAll()
-                    //     println(lines[i])
-                    // }
-                    println(lines[i])
+                for (line in lines) {
+                    if (line.contains('git')) {
+                        // matches = (line =~ /\<(.*?)>/).findAll()
+                        println(line)
+                    }
                 }
             }
         }
