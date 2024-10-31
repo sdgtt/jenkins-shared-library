8fab617393487c74ea375af5f61b1159a4f4d111
Pineda <Julia.Pineda@analog.com>
2024-10-31 12:09:40 +0800

    Parse nebula log
    Signed-off-by: Pineda <Julia.Pineda@analog.com>

--- src/sdg/Gauntlet.groovy
+++ src/sdg/Gauntlet.groovy
@@ -995,12 +995,12 @@ def stage_library(String stage_name) {
                 def keys = ['total', 'used', 'free', 'shared', 'buff/cache', 'available']
                 def memory_type = ['Mem:', 'Swap:']
 
-                for (i = 0; i < lines.size(); i++) {
-                    if (lines[i].contains('git')) {
-                        // matches = (line =~ /\<(.*?)>/).findAll()
-                        println(lines[i])
-                    }
-                }
+                // for (i = 0; i < lines.size(); i++) {
+                //     if (lines[i].contains('git')) {
+                //         // matches = (line =~ /\<(.*?)>/).findAll()
+                //         println(lines[i])
+                //     }
+                // }
             }
         }
         break
