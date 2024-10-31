541d2711d199e8aed30daa98713f4cfb081c501c
Julia Pineda <Julia.Pineda@analog.com>
2024-10-31 14:46:51 +0800

    cat file
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

--- src/sdg/Gauntlet.groovy
+++ src/sdg/Gauntlet.groovy
@@ -990,7 +990,7 @@ def stage_library(String stage_name) {
                 def memory_type = ['Mem:', 'Swap:']
 
                 if (fileExists(log_file)) {
-                    // sh 'cat ' + log_file
+                    sh 'cat ' + log_file
                     def file = readFile log_file
                     lines = file.readLines()
                     // echo lines
