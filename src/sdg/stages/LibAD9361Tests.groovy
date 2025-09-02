65d69ce4a903b53ed34b4f2d76e5df3a230192b0
Trecia Agoylo <trecia.agoylo@analog.com>
2025-09-02 09:58:16 +0800

    stages: add refactored liba9361tests stage and its tests
    Signed-off-by: Trecia Agoylo <trecia.agoylo@analog.com>

---
+++ src/sdg/stages/LibAD9361Tests.groovy
@@ -0,0 +1,78 @@
+package sdg.stages
+import sdg.Gauntlet
+import sdg.stages.IStage
+
+/**
+ * The LibAD9361Tests class implements the IStage interface
+ * and provides functionality to run libad9361 tests on the target board.
+ */
+class LibAD9361Tests implements IStage {
+    /**
+     * Returns the name of the stage.
+     *
+     * @return The name of the stage, which is "LibAD9361Tests".
+     */
+    String getStageName(){
+        return "LibAD9361Tests"
+    }
+
+    /**
+  
+     */
+    Closure getCls(){
+        return { gauntlet, board ->
+            gauntlet.stepExecutor.stage(getStageName()){
+                stageSteps(gauntlet, board)
+            }
+        }
+    }
+    /**
+     * Executes the steps for the LibAD9361Tests stage.
+     *
+     * @param gauntlet The Gauntlet instance used to execute the stage.
+     * @param board The name of the board to be power cycled.
+     */
+    void stageSteps(Gauntlet gauntlet, String board){
+        def logger = gauntlet.logger
+        def gauntEnv = gauntlet.gauntEnv
+        def steps = gauntlet.stepExecutor
+
+        logger.info("Running ${getStageName()} for ${board}")
+        def supported = false
+        def supported_boards = ["adrv9361", "adrv9364", "ad9361", "ad9364", "pluto"]
+        for(s in supported_boards){
+            if (board.contains(s)){
+                supported = true
+            }
+        }
+        if(supported && gauntEnv.libad9361_iio_branch != null){
+            try{
+                def ip = gauntlet.nebula("update-config -s network-config -f dutip --board-name="+board)
+                gauntlet.run_i('sudo rm -rf libad9361-iio')
+                gauntlet.run_i('git clone -b '+ gauntEnv.libad9361_iio_branch + ' ' + gauntEnv.libad9361_iio_repo, true)
+                steps.dir('libad9361-iio')
+                {
+                    steps.sh('mkdir -p build')
+                    steps.dir('build')
+                    {
+                        steps.sh('cmake -DPYTHON_BINDINGS=ON ..')
+                        steps.sh('make')
+                        steps.sh('make install')
+                        steps.sh('ldconfig')
+                        steps.sh('URI_AD9361="ip:'+ip+'" ctest -T test --no-compress-output -V')
+                    }     
+                }
+            }catch(Exception ex){
+                steps.unstable("LibAD9361Tests Failed: ${ex.getMessage()}")
+            }finally{
+                steps.dir('libad9361-iio/build'){
+                    steps.sh("mv Testing ${board}")
+                    xunit([CTest(deleteOutputFiles: true, failIfNotNew: true, pattern: "${board}/**/*.xml", skipNoTestFiles: false, stopProcessingIfError: true)])
+                    steps.archiveArtifacts artifacts: "${board}/**/*.xml", followSymlinks: false, allowEmptyArchive: true
+                }
+            }
+        }else{
+            logger.info("LibAD9361Tests: Skipping board: "+board)
+        }
+    }
+}
---
+++ test/sdg/stages/TestLibAD9361Tests.groovy
@@ -0,0 +1,140 @@
+package sdg.stages
+
+import spock.lang.Specification
+import sdg.Gauntlet
+import sdg.Logger
+import sdg.IStepExecutor
+import sdg.ioc.*
+import groovy.lang.GroovyShell
+
+class TestLibAD9361Tests extends Specification {
+
+    def shell
+    def getGauntEnv
+
+    IStepExecutor steps
+    IContext context
+
+    def setup() {
+        // mock the context
+        shell = new GroovyShell()
+        getGauntEnv = shell.parse(new File('vars/getGauntEnv.groovy'))
+        
+        steps =  Mock(IStepExecutor.class)
+        context = Mock(IContext.class)
+    }
+
+    def "test getStageName"() {
+        given:
+        def libAD9361Tests = new LibAD9361Tests()
+
+        expect:
+        libAD9361Tests.getStageName() == "LibAD9361Tests"
+    }
+
+    def "test getCls"() {
+        given:
+        def libAD9361Tests = new LibAD9361Tests()
+        String board = "zynq-zc702-adv7511-ad9361-fmcomms2-3"
+
+        when:
+        def closure = libAD9361Tests.getCls()
+
+        then:
+        closure instanceof Closure
+    }
+    
+    def "test stageSteps - supported board"() {
+        given:
+        String board = "zynq-zc702-adv7511-ad9361-fmcomms2-3"
+        
+        // Mock gauntlet 
+        context.getStepExecutor() >> steps
+        context.isDefault() >> false
+        
+        // Create gauntEnv with required properties
+        def mockGauntEnv = getGauntEnv.call("NA","NA","NA","v0.31","NA")
+        mockGauntEnv.libad9361_iio_branch = "main"
+        mockGauntEnv.libad9361_iio_repo = "https://github.com/analogdevicesinc/libad9361-iio.git"
+        
+        steps.getGauntEnv(_,_,_,_,_) >> mockGauntEnv
+        steps.isUnix() >> true
+        steps.sh(script: 'uname', returnStdout: true) >> 'Linux'
+        steps.fileExists('out.out') >> true
+        steps.readFile('out.out') >> 'STDOUT of some successful nebula command'
+        
+        // Mock all shell commands that will be called
+        steps.sh('mkdir -p build') >> null
+        steps.sh('cmake -DPYTHON_BINDINGS=ON ..') >> null
+        steps.sh('make') >> null
+        steps.sh('make install') >> null
+        steps.sh('ldconfig') >> null
+        steps.sh({ String cmd -> cmd.contains('URI_AD9361') && cmd.contains('ctest') }) >> null
+        steps.sh("mv Testing ${board}") >> null
+        
+        // Mock dir operations with closure execution
+        steps.dir(_, _) >> { String dirName, Closure closure -> 
+            closure.call()
+        }
+        
+        // Mock other Jenkins pipeline steps
+        steps.unstable(_) >> null
+        steps.archiveArtifacts(_) >> null
+        
+        ContextRegistry.registerContext(context)
+        Gauntlet gauntlet = new Gauntlet()
+        gauntlet.construct("NA","NA","NA","v0.31","NA")
+        
+        // Mock gauntlet methods using metaClass
+        gauntlet.metaClass.run_i = { String cmd, boolean doRetry = false -> 
+            return null  // Mock successful execution
+        }
+        
+        def libAD9361Tests = new LibAD9361Tests()
+        
+        // Override global methods that might be called
+        libAD9361Tests.metaClass.xunit = { def config -> null }
+        libAD9361Tests.metaClass.CTest = { Map params -> [:] }
+        
+        when:
+        libAD9361Tests.stageSteps(gauntlet, board)
+
+        then:
+        1 * steps.sh(['script':'nebula update-config -s network-config -f dutip --board-name=zynq-zc702-adv7511-ad9361-fmcomms2-3', 'returnStdout':true])
+        1 * steps.sh('URI_AD9361="ip:" ctest -T test --no-compress-output -V') // Verify test command is called
+    }
+
+    def "test stageSteps - unsupported board"() {
+        given:
+        String board = "zynq-zc706-adv7511-fmcomms11"
+        
+        // Mock gauntlet 
+        context.getStepExecutor() >> steps
+        context.isDefault() >> false
+        
+        // Create gauntEnv - for unsupported board test, we can use basic gauntEnv
+        def mockGauntEnv = getGauntEnv.call("NA","NA","NA","v0.31","NA")
+        
+        steps.getGauntEnv(_,_,_,_,_) >> mockGauntEnv
+        steps.isUnix() >> true
+        steps.sh(script: 'uname', returnStdout: true) >> 'Linux'
+        steps.fileExists('out.out') >> true
+        steps.readFile('out.out') >> 'STDOUT of some successful nebula command'
+        
+        ContextRegistry.registerContext(context)
+        Gauntlet gauntlet = new Gauntlet()
+        gauntlet.construct("NA","NA","NA","v0.31","NA")
+        
+        // Mock the logger
+        def logger = Mock(sdg.Logger)
+        gauntlet.logger = logger
+
+        def libAD9361Tests = new LibAD9361Tests()
+
+        when:
+        libAD9361Tests.stageSteps(gauntlet, board)
+
+        then:
+        1 * logger.info("LibAD9361Tests: Skipping board: "+board)
+    }
+}
