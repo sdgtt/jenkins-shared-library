dd3f2bf6c832ced14d0c54f45428ac713b104df8
KimChesed-Paller_adi <kimchesed.paller@analog.com>
2026-07-23 14:52:57 +0800

    fix: register default context
    Signed-off-by: KimChesed-Paller_adi <kimchesed.paller@analog.com>

--- vars/getGauntlet.groovy
+++ vars/getGauntlet.groovy
@@ -3,6 +3,7 @@ import sdg.Gauntlet
 
 def call(hdlBranch="NA", linuxBranch="NA", bootPartitionBranch="release",firmwareVersion="NA", bootfile_source="artifactory") {
 
+    ContextRegistry.registerDefaultContext(this)
     def harness =  new Gauntlet()
     harness.construct(hdlBranch, linuxBranch, bootPartitionBranch, firmwareVersion, bootfile_source)
     return harness
